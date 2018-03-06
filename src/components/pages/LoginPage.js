ba91cda027e45f3fb957dff1ab3e77993033da43
shady <huangdongyang@honraytech.com>
2018-03-06 23:25:26 +0800

    login form and client validation

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import LoginForm from '../forms/LoginForm';
 
-const LoginPage = () => (
-    <div>
-        <h1>Ligin Page</h1>
-    </div>
+class LoginPage extends React.Component {
+    submit = data => {
+        console.log(data);
+    };
 
-)
+    render() {
+        return (
+            <div>
+                <h1>Ligin Page</h1>
+                <LoginForm submit={this.submit} />
+            </div>
+        );
+    }
+}
 
 export default LoginPage 
